7ab0e3bda23d67c9016f8a75debe7378f96a737a
neeraj1434 <neerajkumarm345@gmail.com>
2023-04-11 23:36:54 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+        &#x1F600; Welcome to our Todo App! &#x1F600;<br/><br/> This Web-app is designed to streamline your daily tasks and ensure that you never miss an important deadline or forget a crucial task again.
+
+        
+
+        </div>
+    )
+}
